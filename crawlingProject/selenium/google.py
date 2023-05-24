2ed49729d7165682d13872037f59312966339c9b
Man-E5 <dev2jones1@gmail.com>
2023-05-24 23:59:11 +0900

    크롤링 오류 확인 및 코드 정리

--- crawlingProject/selenium/google.py
+++ crawlingProject/selenium/google.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import urllib.request
+import os
 
 
 
@@ -15,13 +16,34 @@ chr_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 #드라이버 설정
 driver = webdriver.Chrome(options=chr_options)
 
+
+#검색 변수 설정
+
+celebName = "박보영"
+maxCap = 100
+
+thumeClass = ".rg_i.Q4LuWd"
+xPath = "/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]"
+
+
+
+#저장 환경 설정
+#지정 이미지 저장 폴더 이름
+outpath = "C:/Users/lenovo/Desktop/crawlingProject/crawlingProject/downImg/" + celebName + "/" 
+#폴더가 존재하지 않는다면 폴더 생성
+if not os.path.isdir(outpath):
+    os.makedirs(outpath)
+
+
+
 #웹페이지 열기
-driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")
+driver.get("https://www.google.co.kr/imghp")
+
 
 
 #검색창 확인 후 검색
 elem = driver.find_element(By.NAME, "q")
-elem.send_keys("와우맨")
+elem.send_keys(celebName)
 elem.send_keys(Keys.RETURN)
 
 
@@ -62,32 +84,56 @@ while True:
 
     last_height = new_height
 
-time.sleep(3)
+time.sleep(5)
 
 #썸네일 선택 구문
-images = driver.find_elements(By.CSS_SELECTOR, ".bRMDJf.islir")
+images = driver.find_elements(By.CSS_SELECTOR, thumeClass)
 
-
-
+print(len(images))
 
 #이름 설정용 변수
 count = 1
+fileName = ""
 
 
 
 #이미지 다운로드 반복문
 for image in images:
 
-    print(image)
-    image.click()
+    # print(image)
     time.sleep(1)
-    imgUrl = driver.find_element(By.XPATH, "/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]").get_attribute("src")
 
+    #오류시 넘어가부려
+    try:
+        image.click()
+    except:
+        count = count -1
+        continue
+    
+    
+    imgUrl = driver.find_element(By.XPATH, xPath).get_attribute("src")
+
+
+    if count < 10:
+        fileName = celebName + "_0" + str(count)
+    else:
+        fileName = celebName + "_" + str(count)
+
+    
     # print(imgUrl)
-    urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+
+
+    #403 회피기동 방법 1
+
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozila/5.0')]
+
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(imgUrl, outpath+ fileName + ".jpg")
+
     count = count + 1
 
-    if count > 10:
+    if count > maxCap:
         break
 
 
@@ -97,4 +143,6 @@ for image in images:
 # elem.send_keys("pycon")
 # elem.send_keys(Keys.RETURN)
 # assert "No results found." not in driver.page_source
-# driver.close()
+
+#아따메 끄기
+driver.close()
